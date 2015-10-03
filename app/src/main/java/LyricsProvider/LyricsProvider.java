702522dc96ae0fb69abcde76d41ccf73c3360d97
nanomader <jacek@musi.al>
2015-10-03 23:48:54 +0200

    added package lyricsProvider and class LyricsSearch

---
+++ app/src/main/java/LyricsProvider/LyricsProvider.java
@@ -0,0 +1,56 @@
+package LyricsProvider;
+
+/**
+ * Created by re on 2015-10-03.
+ */
+import android.os.Handler;
+import android.os.Message;
+import android.util.Log;
+
+import java.io.UnsupportedEncodingException;
+import java.net.URLEncoder;
+
+public abstract class LyricsProvider {
+
+    protected Track mTrack = null;
+    protected String mLyrics = null;
+    protected Handler mHandler = null;
+
+    public static final String TAG = "JLyrProvider";
+
+    public LyricsProvider(Track track) {
+        mTrack = track;
+    }
+
+    public abstract String getSource();
+
+    public String getLyrics() {
+        return mLyrics;
+    }
+
+    protected void doError() {
+        Message msg = Message.obtain(mHandler, Lyrics.DID_ERROR);
+        mHandler.sendMessage(msg);
+    }
+
+    protected void doFail() {
+        Message msg = Message.obtain(mHandler, Lyrics.DID_FAIL);
+        mHandler.sendMessage(msg);
+    }
+
+    protected void doLoad() {
+        Message msg = Message.obtain(mHandler, Lyrics.DID_LOAD);
+        mHandler.sendMessage(msg);
+    }
+
+    public abstract void loadLyrics(Handler _handler);
+
+    protected static String enc(String s) {
+        try {
+            return URLEncoder.encode(s, "UTF-8");
+        } catch (UnsupportedEncodingException e) {
+            Log.e(TAG, "URLEncoder lacks support for UTF-8!?");
+            return null;
+        }
+    }
+}
---
+++ app/src/main/java/al/musi/lyricsfetcher/LyricsSearch.java
@@ -0,0 +1,9 @@
+package al.musi.lyricsfetcher;
+
+import android.app.Activity;
+
+/**
+ * Created by re on 2015-10-03.
+ */
+public class LyricsSearch extends Activity {
+}
